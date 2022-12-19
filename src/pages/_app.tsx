1052184eff6546267dd74a31465a21b61d1f6243
ishibashi0112 <yuki.ishibashi0112@gmail.com>
2022-12-19 14:09:18 +0900

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import "src/styles/globals.css";
+
+import { MantineProvider } from "@mantine/core";
+import type { AppProps } from "next/app";
+import React from "react";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{ colorScheme: "dark" }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+};
+
+export default App;
